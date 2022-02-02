bc2dc0591deb34e2f0e590bcaa6e5dd919124b41
Swati P <swatisp2304@gmail.com>
2022-02-02 18:03:20 -0500

    Project_upload

---
+++ src/p1/Test1.java
@@ -0,0 +1,7 @@
+package p1;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("In P1 test");
+	}
+}
---
+++ src/p2/Test2.java
@@ -0,0 +1,7 @@
+package p2;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("In P2 test");
+}
+}
